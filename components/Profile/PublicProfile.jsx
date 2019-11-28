ad65fc48d61fd048ad3a96037934b26b1fac5d96
BASEL8 <basel84mn@gmail.com>
2019-11-28 11:55:09 +0100

    fix import

--- components/Profile/PublicProfile.jsx
+++ components/Profile/PublicProfile.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import Tabs from '../profile/Tabs'
+import Tabs from './Tabs'
 import { getPublicProfile } from '../../actions/user'
 import { arrayBufferToBase64 } from '../../actions/helpers'
 import { makeStyles } from '@material-ui/core/styles';
